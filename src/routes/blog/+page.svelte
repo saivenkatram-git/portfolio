a4f0b0e11546a8453fc60eb15bbee7a717376ad3
Sai Venkatram <saiv.contact@gmail.com>
2025-10-06 20:18:03 +0530

    made adjustments to the blog for mobile responsiveness

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import Upcoming from '$lib/assets/upcoming_soon.png?enhanced';
+	import { Motion } from 'svelte-motion';
 </script>
 
-<div class="mx-auto flex h-[1000px] max-w-3xl flex-col items-center justify-center">
-	<enhanced:img src={Upcoming} alt="" class="h-[550px] w-[550px] object-contain" />
-	<p class="text-lg text-purple-400 dark:text-yellow-400">Welcome to the blog page!</p>
-	<p class="text-center text-lg">This page is upcoming and will be deployed very soon 🚀</p>
-</div>
+<Motion initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1 }} let:motion
+	><div
+		use:motion
+		class="mx-auto flex h-[700px] max-w-3xl flex-col items-center justify-center lg:h-[1000px]"
+	>
+		<enhanced:img src={Upcoming} alt="" class="h-[550px] w-[550px] object-contain" />
+		<p
+			class="font-cirka
+      text-2xl text-purple-400 sm:text-3xl lg:text-4xl dark:text-yellow-400"
+		>
+			COMING SOON...
+		</p>
+	</div>
+</Motion>
