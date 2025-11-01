2eb8a77684f4b01fd7ba26e6ce3c1a8dc38373f5
Sai Venkatram <saiv.contact@gmail.com>
2025-11-01 18:01:51 +0530

    fix: removed unncessary margin spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-	import Hero from '$lib/components/+hero.svelte';
-	import AboutMe from '$lib/components/+about-me.svelte';
-	import Work from '$lib/components/+work.svelte';
-	import Projects from '$lib/components/+projects.svelte';
-	import Tech from '$lib/components/+tech.svelte';
-	import Footer from '$lib/components/+footer.svelte';
+	import Hero from '$lib/components/sections/+hero.svelte';
+	import AboutMe from '$lib/components/sections/+about-me.svelte';
+	import Work from '$lib/components/sections/+work.svelte';
+	import Projects from '$lib/components/sections/+projects.svelte';
+	import Tech from '$lib/components/sections/+tech.svelte';
+	import Footer from '$lib/components/sections/+footer.svelte';
 </script>
 
-<div class="mx-auto mt-20 flex max-w-3xl flex-col items-center">
+<div class="mx-auto flex max-w-3xl flex-col items-center">
 	<Hero />
 	<AboutMe />
 	<Work />
